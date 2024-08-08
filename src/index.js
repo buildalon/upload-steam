b1a15a40c7d57ea6560a6e2dbc9482c39f752e88
Stephen Hodgson <rage.against.the.pixel@gmail.com>
2024-08-08 13:24:23 -0400

    upload-steam@v1.0.0 (#1)
    - Initial Release! 🎉🚀

---
+++ src/index.js
@@ -0,0 +1,35 @@
+const core = require('@actions/core');
+const upload = require('./upload');
+const auth = require('./auth');
+
+const STEAM_DIR = process.env.STEAM_DIR;
+const STEAM_CMD = process.env.STEAM_CMD;
+const STEAM_TEMP = process.env.STEAM_TEMP;
+
+const IS_POST = !!core.getState('isPost');
+
+const main = async () => {
+    try {
+        if (!STEAM_DIR) {
+            throw new Error('STEAM_DIR is not defined.');
+        }
+        if (!STEAM_CMD) {
+            throw new Error('STEAM_CMD is not defined.');
+        }
+        if (!STEAM_TEMP) {
+            throw new Error('STEAM_TEMP is not defined.');
+        }
+        if (!IS_POST) {
+            core.saveState('isPost', 'true');
+            const isLoggedIn = await auth.IsLoggedIn();
+            if (!isLoggedIn) {
+                await auth.Login();
+            }
+            await upload.Run();
+        }
+    } catch (error) {
+        core.setFailed(error);
+    }
+}
+
+main();
