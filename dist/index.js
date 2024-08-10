da2ddaa59ea01cfafb082676fe4a71f5a2e2da02
Stephen Hodgson <rage.against.the.pixel@gmail.com>
2024-08-10 15:34:50 -0400

    upload-steam@v1.0.1 (#3)
    - removed redundant post step

--- dist/index.js
+++ dist/index.js
@@ -28534,8 +28534,6 @@ const STEAM_DIR = process.env.STEAM_DIR;
 const STEAM_CMD = process.env.STEAM_CMD;
 const STEAM_TEMP = process.env.STEAM_TEMP;
 
-const IS_POST = !!core.getState('isPost');
-
 const main = async () => {
     try {
         if (!STEAM_DIR) {
@@ -28547,14 +28545,11 @@ const main = async () => {
         if (!STEAM_TEMP) {
             throw new Error('STEAM_TEMP is not defined.');
         }
-        if (!IS_POST) {
-            core.saveState('isPost', 'true');
-            const isLoggedIn = await auth.IsLoggedIn();
-            if (!isLoggedIn) {
-                await auth.Login();
-            }
-            await upload.Run();
+        const isLoggedIn = await auth.IsLoggedIn();
+        if (!isLoggedIn) {
+            await auth.Login();
         }
+        await upload.Run();
     } catch (error) {
         core.setFailed(error);
     }
